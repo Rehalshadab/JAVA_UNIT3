baaa9a6329afd7810a6bfe3bb7e5f5bace757f06
shadab rehal <rehalon786@gmail.com>
2026-01-29 12:32:34 +0530

    Add CharWriteDemo to demonstrate file writing

---
+++ SOURCE/CharWriteDemo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class CharWriteDemo {
+
+public static void main(String[] args) throws IOException {
+FileWriter fw = new FileWriter("text.txt");
+fw.write("Welcome to Java File Handling");
+fw.close();
+System.out.println("Text written using character stream");
+}
+}
