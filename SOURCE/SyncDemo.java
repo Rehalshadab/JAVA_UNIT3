3b4422bbf6a2f79ab7d77c51ccba314fc042b4eb
shadab rehal <rehalon786@gmail.com>
2026-01-29 12:36:50 +0530

    Create SyncDemo.java

---
+++ SOURCE/SyncDemo.java
@@ -0,0 +1,24 @@
+class Counter {
+int count = 0;
+synchronized void increment() {
+count++;
+}
+}
+class SyncDemo extends Thread {
+static Counter c = new Counter();
+public void run() {
+for(int i = 0; i < 1000; i++) {
+c.increment();
+}
+}
+public static void main(String[] args) throws InterruptedException {
+SyncDemo t1 = new SyncDemo();
+SyncDemo t2 = new SyncDemo();
+t1.start();
+t2.start();
+t1.join();
+t2.join();
+System.out.println("Final count: " + c.count);
+
+}
+}
