9bcfb8620cf8cb57b6fa1d3a0231cd29d52df051
shadab rehal <rehalon786@gmail.com>
2026-01-29 12:34:51 +0530

    Create FileCreateDemo.java

---
+++ SOURCE/FileCreateDemo.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreateDemo {
+    public static void main(String[] args) {
+        try {
+            File f = new File("newfile.txt");
+
+            if (f.createNewFile()) {
+                System.out.println("File created successfully");
+            } else {
+                System.out.println("File already exists");
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+        }
+    }
+}
